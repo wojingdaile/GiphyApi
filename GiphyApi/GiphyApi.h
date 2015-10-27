1cf6858542f20df4304fcf083638ea7aa943cc4d
wojingdaile <tangyafeng1990@gmail.com>
2015-10-27 15:25:09 +0800

    init project
    init project as a library.

---
+++ GiphyApi/GiphyApi.h
@@ -0,0 +1,19 @@
+//
+//  GiphyApi.h
+//  GiphyApi
+//
+//  Created by TangYafeng on 15/10/27.
+//  Copyright © 2015年 Yafeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GiphyApi.
+FOUNDATION_EXPORT double GiphyApiVersionNumber;
+
+//! Project version string for GiphyApi.
+FOUNDATION_EXPORT const unsigned char GiphyApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GiphyApi/PublicHeader.h>
+
+
